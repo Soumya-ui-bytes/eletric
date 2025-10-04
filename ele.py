b6e532f0068f166311616b544874306bcb0f5173
Soumya-ui-bytes <soumyarajagopal027@gmail.com>
2025-10-04 11:40:51 +0530

    saved

--- ele.py
+++ ele.py
@@ -1,3 +1,8 @@
 ele= int(input("Enter the electricity consumed: "))
 total_ele= (ele * 5)
 print(f"the total bill is :{total_ele}")
+if total_ele > 1000:
+
+discount = (total_ele *0.10)
+total_ele -= discount
+print(f"the discounted bill after exceeding 1000ru :{discount}")
